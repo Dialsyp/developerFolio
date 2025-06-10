445680b28268256f6bd3c5315f9eec17d7eab781
dialsy <dialsyphax@gmail.com>
2025-06-10 13:53:51 +0200

    Mise à jour

--- src/portfolio.js
+++ src/portfolio.js
@@ -84,7 +84,7 @@ const educationInfo = {
   schools: [
     {
       schoolName: "CESI École d’Ingénieurs",
-      logo: require("./assets/images/Cesi.jpg"), 
+      logo: require("./assets/images/Cesi.jfif"), 
       subHeader:
         "Manager en Architecture et Applications Logicielles des SI (alternance)",
       duration: "Nov 2023 - Oct 2025",
